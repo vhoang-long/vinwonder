e6f1a6ca8066cb3c872b76250412946afaedb04d
NTGNguyen <ntgnguyen@duck.com>
2025-11-14 19:05:20 +0700

    fix: next

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -122,7 +122,7 @@ function initChatbot() {
     msg.className = `message ${isUser ? "user-message" : "bot-message"}`;
     if (!isUser) {
       const avatar = document.createElement("img");
-      avatar.src = "./assets/Mascot VinWonders Nha Trang.png";
+      avatar.src = "../assets/avartar2.png";
       avatar.alt = "Bé Vin";
       avatar.className = "message-avatar";
       avatar.onerror = function () {
